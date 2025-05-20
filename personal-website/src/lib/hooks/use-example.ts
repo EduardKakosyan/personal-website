d8a39a986de6a20396663dc16e2ad73a6f24834a
Eduard Kakosyan <90533005+EduardKakosyan@users.noreply.github.com>
2025-05-20 10:17:06 -0300

    init: folder structure template

---
+++ personal-website/src/lib/hooks/use-example.ts
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+
+// Example custom hook
+export function useExampleHook(initialValue: any) {
+  const [value, setValue] = useState(initialValue);
+  useEffect(() => {
+    // Some effect
+  }, [value]);
+  return [value, setValue];
+} 
